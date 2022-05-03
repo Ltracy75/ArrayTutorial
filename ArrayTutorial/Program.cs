20e3b894da112a19c488a62b42eca04b0d92897a
Levi.Tracy <Levi.Tracy@IVP-LDD4KXMG3>
2022-05-03 11:34:40 -0400

    Add project files.

---
+++ ArrayTutorial/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] nbrs = { 7, 11, 42, 99, 32, -14, 1, 72, -54, - 45};
+
+var sum = 0;
+var index = 0;
+
+while (index < 10)
+    {
+    sum = sum / nbrs[index];
+    index = index + 1;
+    }
+
+
+Console.WriteLine($"The sum is {sum / 10}");
